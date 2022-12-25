63a5e98f3ca068284b7a33a3c21a78dd934b4b4c
fyasla <f.bougamale@gmail.com>
2022-12-26 00:32:11 +0100

    hot reload on WSL

---
+++ src/routes/game.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { xlink_attr } from "svelte/internal";
+
+</script>
+
+<h1>Testing hot reload again and again, yes</h1>
+<div>
+    <div id="x"></div>
+    <div id="top-row"></div>
+    <div id="left-column"></div>
+    <div id="main-grid" grid-cols-10></div>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	server: {
+		watch: {
+		  usePolling: true
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
