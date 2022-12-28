0c9e752efa870f23e26535447d824c3d29837367
fyasla <f.bougamale@gmail.com>
2022-12-28 03:40:25 +0100

    move timer in own component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
     import Game from './Game.svelte';
-    //create a timer
-    let timer: number = 0;
-    //uspdate the timer every second
-    setInterval(() => {
-        timer++;
-    }, 1000);
+	import Timer from './Timer.svelte';
 </script>
 
 <slot>
     <h1>FREECH</h1>
-    <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
-        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-            <span class="countdown font-mono text-5xl">
-            <span style="--value:{timer};"></span>
-            </span>
-            min
-        </div> 
-        <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-            <span class="countdown font-mono text-5xl">
-            <span style="--value:{timer};"></span>
-            </span>
-            sec
-        </div>
-    </div>
+    <Timer/>
     <Game />
     <button class="btn btn-primary">Play</button>
     <div class="stat">
---
+++ src/routes/Timer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    //create a timer
+    let timer: number = 0;
+    //uspdate the timer every second
+    setInterval(() => {
+        timer++;
+    }, 1000);
+</script>
+
+<div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+        <span style="--value:{timer};"></span>
+        </span>
+        min
+    </div> 
+    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <span class="countdown font-mono text-5xl">
+        <span style="--value:{timer};"></span>
+        </span>
+        sec
+    </div>
+</div>
