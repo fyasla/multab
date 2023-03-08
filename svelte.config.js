5990df213249875881a283cdc4a2d61a251f2426
fyasla <f.bougamale@gmail.com>
2023-03-08 07:25:08 +0100

    build as static site

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 // import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 // /** @type {import('@sveltejs/kit').Config} */
 // const config = {
@@ -10,14 +9,19 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 // 	kit: {
 // 		adapter: adapter()
 // 	}
-// };
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
  
 export default {
-	preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
   }
 };
-
 //export default config;
